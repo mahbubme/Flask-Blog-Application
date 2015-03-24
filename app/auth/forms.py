b278428fdc2c8f23ba8efd123088aea3637a8fc1
Mahbub <mahbuburrahmanmihir@gmail.com>
2015-03-24 03:08:47 -0700

    Implementation Sign In route

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,6 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Email
+from wtforms.validators import Required, Length, Email
 
 
 class LoginForm(Form):
