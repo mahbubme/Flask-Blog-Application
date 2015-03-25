74b9d2d003c8843e9eb550f8461d91c7a2bf55d7
Mahbub <mahbuburrahmanmihir@gmail.com>
2015-03-25 16:56:33 -0700

    Password updates

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -29,5 +29,13 @@ class RegistrationForm(Form):
 			raise ValidationError('Username already in use.')
 
 
+class ChangePasswordForm(Form):
+	old_password = PasswordField('Old password', validators=[Required()])
+	password = PasswordField('New password', validators=[Required(), EqualTo('password2', message='Passwords must match')])
+	password2 = PasswordField('Confirm new password', validators=[Required()])
+	submit = SubmitField('Update Password')
+
+
+
 
 
