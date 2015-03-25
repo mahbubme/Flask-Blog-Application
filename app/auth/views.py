befc621e4a3d37c19530c24b9e0ab716926d9bb7
Mahbub <mahbuburrahmanmihir@gmail.com>
2015-03-25 03:15:08 -0700

    Confirm a user account

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask.ext.login import login_user, logout_user, login_required
+from flask.ext.login import login_user, logout_user, login_required, current_user
 from . import auth
 from ..models import User
 from .forms import LoginForm, RegistrationForm
@@ -38,3 +38,15 @@ def register():
 		flash('A confirmation email has been sent to you by email.')
 		return redirect(url_for('main.index'))
 	return render_template('auth/register.html', form=form)
+
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+	if current_user.confirmed:
+		return redirect(url_for(main.index))
+	if current_user.confirm(token):
+		flash('You have confirmed your account. Thanks!')
+	else:
+		flash('The confirmation link is invalid or has expired.')
+	return redirect(url_for('main.index'))
