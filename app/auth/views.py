b055e3f66d1a9caaf6a85e7ebe49eee50ae8edc8
Mahbub <mahbuburrahmanmihir@gmail.com>
2015-03-25 15:31:44 -0700

    Filter unconfirmed accounts in before_app_request handler

--- app/auth/views.py
+++ app/auth/views.py
@@ -18,6 +18,19 @@ def login():
 	return render_template('auth/login.html', form=form)
 
 
+@auth.before_app_request
+def before_request():
+	if current_user.is_authenticated() and not current_user.confirmed and request.endpoint[:5] != 'auth.':
+		return redirect(url_for('auth.unconfirmed'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+	if current_user.is_anonymous() or current_user.confirmed:
+		return rdirect('main.index')
+	return render_template('auth/unconfirmed.html')
+
+
 @auth.route('/logout')
 @login_required
 def logout():
