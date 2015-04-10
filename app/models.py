5747f27750850fbecccdeaa357da4b569de335b9
Mahbub <mahbuburrahmanmihir@gmail.com>
2015-04-10 15:40:29 -0700

    Create roles in the database

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class Permission:
     COMMENT = 0x02
     WRITE_ARTICLES = 0x08
     ADMINISTER = 0x80
-    
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -19,6 +19,27 @@ class Role(db.Model):
     default = db.Column(db.Boolean, default=False, index=True)
     permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User' : (Permission.FOLLOW | 
+                      Permission.COMMENT | 
+                      Permission.WRITE_ARTICLES, True),
+            'Moderator' : (Permission.FOLLOW | 
+                           Permission.COMMENT | 
+                           Permission.WRITE_ARTICLES | 
+                           Permission.MODERATE_COMMENTS, False),
+            'Administrator' : (0xff, False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
 
     def __repr__(self):
         return '<Role %r>' % self.name
